88d58b457153018b68580b4ca853526008ef522d
Arnaud Spiwack <arnaud@spiwack.net>
2019-09-04 22:37:02 +0200

    feat: num instance for Fin n

--- src/Lib.hs
+++ src/Lib.hs
@@ -39,6 +39,14 @@ mkFin i =
 instance Show (Fin n) where
   show (Fin i) = show i
 
+instance KnownNat n => Num (Fin n) where
+  fromInteger = mkFin . fromInteger
+  (+) = error "Not implemented"
+  (*) = error "Not implemented"
+  abs = error "Not implemented"
+  signum = error "Not implemented"
+  negate = error "Not implemented"
+
 -- >>> [0..9]
 -- [0,1,2,3,4,5,6,7,8,9]
 
